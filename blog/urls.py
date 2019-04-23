4ce869cda942c5cd5ebe8a96d0dccb165765664a
mongdolappa <seungjae1164@gmail.com>
2019-04-23 12:02:08 +0900

    added view_detail

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,14 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
-# 이제 post_list라는 view가 루트 URL에 할당되었습니다. '' 루트 디렉토리에 View의 post_list함수가 (이 안에 가르키는 html실행) 할당됨.
-# 이 패턴은 장고에게 누군가 웹사이트에 'http://127.0.0.1:8000/' 주소로 들어왔을 때 views.post_list를 보여주라고 말해줍니다.
-# 마지막 부분인 name='post_list'는 URL에 이름을 붙인 것으로 뷰를 식별합니다. 뷰의 이름과 같을 수도 완전히 다를 수도 있습니다.
+# 이제 post_list라는 view가 루트 URL에 할당되었습니다. ''
+# 루트 디렉토리에 View의 post_list함수가 (이 안에 가르키는 html실행) 할당됨.
+# 이 패턴은 장고에게 누군가 웹사이트에 'http://127.0.0.1:8000/' 주소로 들어왔을 때
+# views.post_list를 보여주라고 말해줍니다.
+# 마지막 부분인 name='post_list'는 URL에 이름을 붙인 것으로 뷰를 식별합니다.
+# 뷰의 이름과 같을 수도 완전히 다를 수도 있습니다.
+
+# 브라우저에 http://127.0.0.1:8000/post/5/라고 입력하면,
+# 장고는 post_detail 뷰를 찾아 매개변수 pk가 5인 값을 찾아 뷰로 전달합니다.
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post  #blog.models 로 해도 되나, 동일 디렉토리 내는 .models
 
 # Create your views here.
 def post_list(request):
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts = Post.objects.all
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 # 방금 post_list라는 함수(def)를 만들었습니다.
 # 이 함수는 요청(request)을 넘겨받아 render메서드를 호출합니다.
 # 이 함수는 render 메서드를 호출하여 받은(return) blog/post_list.html 템플릿을 보여줍니다.
 
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html',{'post':post})
